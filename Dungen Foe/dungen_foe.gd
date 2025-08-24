56489cfa8bf39db9d754a1efa345dffccc1ce3b9
THE21Falcam <debanjanpahari09@gmail.com>
2025-08-24 11:13:01 +0530

    Added Atttack and Damage Mechanics to the Game

--- Dungen Foe/dungen_foe.gd
+++ Dungen Foe/dungen_foe.gd
@@ -1,1 +1,34 @@
 extends Node2D
+
+var HP = 20
+var Foe_Die = false
+var Foe_hit = false
+@onready var sprite = $AnimatedSprite2D
+var hp: int = 100
+var state: String = "idle"
+	
+func play_idle():
+	state = "idle"
+	sprite.play("Demon_Normal")
+
+func play_attack():
+	state = "attack"
+	sprite.play("Demon_Hurt")
+
+func play_die():
+	state = "die"
+	sprite.play("Demon_Die")
+	
+
+func _process(delta: float) -> void:
+	# Check health
+	if HP <= 0 and state != "die":
+		Foe_Die = true
+		play_die()
+
+	if state == "attack" and not sprite.is_playing():
+		play_idle()
+
+	if state == "die" and not sprite.is_playing():
+		
+		sprite.hide()
--- Main World/main_world.gd
+++ Main World/main_world.gd
@@ -1,1 +1,17 @@
 extends Node2D
+
+@onready var Enemy = $"Dungen Foe"
+@onready var For_HP_Bar = $"Extra UI/Dungen GUI/Foe HP Bar"
+@onready var Attack_Button = $"Extra UI/Dungen GUI/GridContainer/Attack"
+
+func _process(delta: float) -> void:
+	For_HP_Bar.value = Enemy.HP
+	Attack_Button.disabled = Enemy.Foe_Die
+	
+func _on_attack_pressed() -> void:
+	if Enemy.state != "die":
+		Enemy.play_attack()
+	Enemy.HP -= 1
+	
+	
+	
